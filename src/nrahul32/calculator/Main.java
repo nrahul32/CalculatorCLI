e103064626ec6a1fd24c383d4189f6e5fd297e26
Rahul N <rahul.eee32@gmail.com>
2020-05-04 13:08:00 +0530

    used parallel arrays and for loops to calculate multiple entries

--- src/nrahul32/calculator/Main.java
+++ src/nrahul32/calculator/Main.java
@@ -4,39 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Initializing the numbers and operator
-        double val1 = 100.0d;
-        double val2 = 20.0d;
-        double result;
-        char opCode = 'd';
-        // a - addition, can use d,m,a,s
+        // Initializing the numbers and operator using parallel arrays
+        double[] left_values = {1.0d, 10.0d, 100.0d, 500.0d};
+        double[] right_values = {0.20d, 2.0d, 20.0d, 100.0d};
+        double[] results = new double[4];
+        char[] opCodes = {'d', 'm', 'a', 's'};
 
         // Calculating
-        if(opCode == 'a'){
-            result = val1 + val2;
-        } else if(opCode == 's'){
-            result = val1 - val2;
-        } else if(opCode == 'm'){
-            result = val1 * val2;
-        } else if(opCode == 'd'){
-            // Ternary operator
-            result = (val2 == 0 ? 0 : val1/val2);
-
-//            Or using if-else:
-//            if(val2 == 0){
-//                System.out.println("val2 cannot be 0");
-//                result = 0;
-//            } else {
-//                result = val1 / val2;
-//            }
-        } else {
-            System.out.println("Invalid opCode: " + opCode);
-            result = 0.0d;
+        for(int i = 0; i<4; i++){
+            if(opCodes[i] == 'a'){
+                results[i] = left_values[i] + right_values[i];
+            } else if(opCodes[i] == 's'){
+                results[i] = left_values[i] - right_values[i];
+            } else if(opCodes[i] == 'm'){
+                results[i] = left_values[i] * right_values[i];
+            } else if(opCodes[i] == 'd'){
+                // Ternary operator
+                results[i] = (right_values[i] == 0 ? 0 : left_values[i]/right_values[i]);
+            } else {
+                System.out.println("Invalid opCode: " + opCodes[i]);
+                results[i] = 0.0d;
+            }
         }
 
-        // Printing the result
-        System.out.println(result);
-
+        // Printing the results
+        for(double result : results){
+            System.out.println(result);
+        }
 
     }
 }
